fddb553dff9850b3c4a3f0f474538b9504fd4c2e
k-roychoudhury <lanuks97@gmail.com>
2025-06-26 16:51:01 +0530

    feat: impl application lifecycle

--- app/core/app.py
+++ app/core/app.py
@@ -1,7 +1,38 @@
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI
+from typing import AsyncGenerator
+from contextlib import asynccontextmanager
 
-from .router import v1_router
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker
+
+from app.core.router import v1_router
+from app.config import settings
+
+
+@asynccontextmanager
+async def application_lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    """
+    Lifespan event handler for the FastAPI application.
+
+    This function is called when the application starts and stops.
+    It can be used to perform startup and shutdown tasks.
+
+    Args:
+        app (FastAPI): The FastAPI application instance.
+
+    Yields:
+        None: This function does not yield any values.
+    """
+    app.state._db_engine = create_async_engine(settings.DB_DSN, echo=settings.DEBUG)
+    app.state._db_session = async_sessionmaker(
+        app.state._db_engine, expire_on_commit=False
+    )
+    
+    yield  # Startup tasks can be added here
+    
+    # Shutdown tasks can be added here if needed
+    await app.state._db_engine.dispose()
 
 
 def application_factory() -> FastAPI:
@@ -15,6 +46,7 @@ def application_factory() -> FastAPI:
         title="luxtj backend",
         description="stateless backend process for luxtj",
         version="0.0.1alpha",
+        lifespan=application_lifespan,
     )
 
     # Configure CORS
